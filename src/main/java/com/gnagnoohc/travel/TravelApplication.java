5bcda6c43f64606721d379a2f0d441386f06a338
beautiyj <thdtkfl328@gmail.com>
2026-07-15 14:18:34 +0900

    feat: 미결제 예약 자동 만료 스케줄러 구현
    - 1분 주기로 '예약중' 상태 30분 경과 건을 '예약만료'로 일괄 전환
    - TravelApplication에 @EnableScheduling 추가 (누락 시 스케줄러 미동작)
    - 만료 처리 건수 로그 출력

--- src/main/java/com/gnagnoohc/travel/TravelApplication.java
+++ src/main/java/com/gnagnoohc/travel/TravelApplication.java
@@ -2,8 +2,10 @@ package com.gnagnoohc.travel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling   // 예약 만료 스케줄러(@Scheduled) 활성화
 public class TravelApplication {
 
 	public static void main(String[] args) {

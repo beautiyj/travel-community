9cd53d65f5d1f7386af63642dc6b432c1e0ad8f2
jangcheolsun00 <jangcheolsun00@gmail.com>
2026-07-16 18:13:42 +0900

    feat: 새로 클론 후 커밋

---
+++ src/main/java/com/gnagnoohc/travel/mypage/repository/MypageRepository.java
@@ -0,0 +1,19 @@
+package com.gnagnoohc.travel.mypage.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gnagnoohc.travel.mypage.dto.MypageDto;
+
+@Mapper
+public interface MypageRepository {
+
+    MypageDto getMemberInfo(Long memberId);
+
+    void updateMember(MypageDto member);
+
+    void changePassword(MypageDto member);
+
+    List<MypageDto> getReservationList(Long memberId);
+}

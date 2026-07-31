725e6b527ac5e3d2a7121df65fd830aee750d83d
beautiyj <beautiyj@naver.com>
2026-07-31 15:59:59 +0900

    0731 16:00 임시커밋

--- src/main/java/com/gnagnoohc/travel/tour/mapper/TourMapper.java
+++ src/main/java/com/gnagnoohc/travel/tour/mapper/TourMapper.java
@@ -1,6 +1,9 @@
 package com.gnagnoohc.travel.tour.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.gnagnoohc.travel.tour.model.PlaceDTO;
 import com.gnagnoohc.travel.tour.model.PlaceImageDTO;
@@ -20,4 +23,10 @@ public interface TourMapper {
 
     // PLACE 조회 (ResultMap/ResultType -> PlaceDTO 반환)
     PlaceDTO selectPlaceById(Long placeId);
+
+    // 0731지역 코드 기반 장소 목록 조회 (TourService의 selectByAreaCode 에러 해결용)
+    List<com.gnagnoohc.travel.tour.model.PlaceEntity> selectByAreaCode(@Param("areaCode") String areaCode);
+
+    // 0731 타입별 및 지역별 장소 목록 통합 조회 (TourController의 getPlaceList 에러 해결용)
+    List<PlaceDTO> selectPlaceList(@Param("placeType") String placeType, @Param("regionId") Integer regionId);
 }
